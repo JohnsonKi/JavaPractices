eb360eabb4aa1a5f87d7d324f81c1f2d0f923aed
JohnsonKi <jxm930@gmail.com>
2018-04-07 20:12:23 +0900

    netty sample

---
+++ src/com/netty/sample/sample2/EchoServerHandler.java
@@ -0,0 +1,24 @@
+package com.netty.sample.sample2;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * Handles a server-side channel.
+ */
+public class EchoServerHandler extends ChannelInboundHandlerAdapter { // (1)
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
+        // echo the received data back.
+    	ctx.write(msg);
+    	ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
